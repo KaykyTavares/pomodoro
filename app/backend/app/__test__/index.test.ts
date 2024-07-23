9515d1892f2332099d66c587aa390ad19c59c51c
Kayky Tavares <kayky.programmer@gmail.com>
2024-07-23 18:49:48 -0300

    style: :art: Style the code
    - Eslint fixed the code

--- app/backend/app/__test__/index.test.ts
+++ app/backend/app/__test__/index.test.ts
@@ -1,9 +1,9 @@
 function sum(a: number, b: number): number {
-    return a + b;
+  return a + b;
 }
 
 describe("teste", () => {
-    it(" Must retunr a number", () => {
-        expect(sum(2, 2)).toBe(4)
-    })
-})
+  it(" Must retunr a number", () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+});
--- app/backend/app/src/index.ts
+++ app/backend/app/src/index.ts
@@ -1,13 +1,12 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
 
-const app = express()
+const app = express();
 const port = 4000;
 
 app.get("/", (req: Request, res: Response): void => {
-    res.status(200).send("<h1>teste<h1>")
-    return;
-})
+  res.status(200).send("<h1>teste<h1>");
+});
 
 app.listen(port, () => {
-    console.log(`server up and running on http://localhost:${port}`)
-})
+  console.log(`server up and running on http://localhost:${port}`);
+});
